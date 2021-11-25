53f1e4ff53d280999c3a68d740e93bb59d447acd
Patricia Bird <patriciabird@Patricias-MacBook-Air.local>
2021-11-24 22:21:14 -0500

    add init files + light pseudocode

---
+++ tictactoe.js
@@ -0,0 +1,14 @@
+// module game
+    // turn
+    // reset
+    // start
+    // winner
+// module gameboard
+    // array of board
+    // add piece to board
+    // board check
+// module gamedisplay ?? part of game or solo??
+    //dom manip
+// factory player
+    // name
+    // user input
