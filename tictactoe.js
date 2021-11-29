1400d9080f454c3f08314b04c79e383491fc117d
Patricia Bird <patriciabird@Patricias-MacBook-Air.local>
2021-11-29 10:25:03 -0500

    boardcheck neighbors

--- tictactoe.js
+++ tictactoe.js
@@ -14,25 +14,47 @@ var gameBoard = (() => {
         console.log("Me: " + row + " " + col)
         let size = board.length - 1;
 
-        for (let check = 0; check <= 2; check++) {
-            //check cols
-            if (board[check][0] == board[check][1] && board[check][1] == board[check][2] && board[check][0] == game.getPlayerTurn().getName()) {
-                return true;
+        for (let rowCheck = -1; rowCheck <= 1; rowCheck++) {
+            for (let colCheck = -1; colCheck <= 1; colCheck++) {
+                let rowNeighbor = row + rowCheck;
+                let colNeighbor = col + colCheck;
+                if ((rowNeighbor <= size && rowNeighbor >= 0) && (colNeighbor <= size && colNeighbor >= 0)) {
+                    if (!((rowCheck == 0) && (colCheck == 0))) {
+                        if (board[rowNeighbor][colNeighbor] == game.getPlayerTurn().getName()) {
+                            let thirdPieceRow = -1;
+                            let thirdPieceCol = -1;
+                            if (row == rowNeighbor) {
+                                thirdPieceRow = row;
+                                thirdPieceCol = getThirdPiece(col, colNeighbor)
+                            } else if (col == colNeighbor) {
+                                thirdPieceCol = col;
+                                thirdPieceRow = getThirdPiece(row, rowNeighbor)
+                            }
+                            if (thirdPieceCol == -1 || thirdPieceRow == -1) {
+                                // diagonal?!
+                                return false;
+                            } else if (board[thirdPieceRow][thirdPieceCol] == game.getPlayerTurn().getName()) {
+                                return true;
+                            }
+                        }
+                    }
+                }
             }
-            //check rows
-            if (board[0][check] == board[1][check] && board[1][check] == board[2][check] && board[0][check] == game.getPlayerTurn().getName()) {
-                return true
-            }
-        }
-        //check diagonal top left to bottom right
-        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] == game.getPlayerTurn().getName()) {
-            return true;
-        }
-        //check diagonal top right to bottom left
-        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] == game.getPlayerTurn().getName()) {
-            return true;
         }
         return false;
+    }
+    function getThirdPiece(position, pNeighbor){
+        let thirdPiece = -1;
+        if (position == 0 || pNeighbor == 0) {
+            if (position == 1 || pNeighbor == 1) {
+                thirdPiece = 2;
+            } else {
+                thirdPiece = 1;
+            }
+        } else {
+            thirdPiece = 0;
+        }
+        return thirdPiece;
     }
 
     return {
